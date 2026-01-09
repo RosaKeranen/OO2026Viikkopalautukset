a28739bccdc5693613c785c0258b0a90b3989369
RosaKeranen <rosa.vilkama@gmail.com>
2026-01-09 09:47:37 +0200

    Week 1 coding task done.

--- Viikkoteht_1/Viikkotehtava1/main.cpp
+++ Viikkoteht_1/Viikkotehtava1/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+int game(int maxnum) {
+    int arvattuLuku;
+    int satunnaisluku;
+    int arvaustenMaara = 0;
+
+    srand(time(NULL));
+    satunnaisluku = rand() % maxnum + 1; // 1-maxnum väliltä arvotaan lukuja
+    //cout << satunnaisluku << endl;
+    //Yllä olevalla rivillä testasin printaamalla, että tulee arvotuksi oikeanlainen luku
+
+    do {
+        cout << "Kirjoita numero:";
+        cin >> arvattuLuku;
+        arvaustenMaara++;
+
+        if (arvattuLuku < satunnaisluku) {
+            cout << "Liian pieni.";
+        } else if (arvattuLuku > satunnaisluku) {
+            cout << "Liian suuri.";
+        }
+    } while (arvattuLuku != satunnaisluku);
+
+    cout << "Oikein arvattu!" << endl;
+
+    return arvaustenMaara;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int tulos = game(5);
+    cout << "Arvausten maara: " << tulos << endl;
     return 0;
 }
