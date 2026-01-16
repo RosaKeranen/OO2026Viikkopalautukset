94a96ae6274cd4811817009b22de627657493230
RosaKeranen <rosa.vilkama@gmail.com>
2026-01-16 11:02:18 +0200

    Week 2 coding task done.

--- Viikkoteht_2/Viikkotehtava2/game.cpp
+++ Viikkoteht_2/Viikkotehtava2/game.cpp
@@ -7,26 +7,28 @@ using namespace std;
 
 Game::Game(int max)
 {
-    maxNumber;
-    playerGuess;
-    randomNumber;
+    maxNumber = max;
     numOfGuesses = 0;
+    cout << "Debuggaus-tulostusta. GAME CONSTRUCTOR: object initialized with " << maxNumber << " as a maximum value." << endl;
+
+    //Käyttäjä ei ole vielä antanut omaa maxNumberia.
+    //Joten tässä vaiheessa tulostuu äsken maxNumber-muuttujaan alustettu luku 0.
 }
 
 Game::~Game()
 {
-
+    cout << "Debuggaus-tulostusta. GAME DESTRUCTOR: object cleared from stack memory." << endl;
 }
 
 void Game::play()
 {
+    cout << "Debuggaus-tulostusta. Tama on Game::play -funktio." << endl;
     cout << "Give your maximum number: " << endl;
     cin >> maxNumber;
 
     srand(time(NULL));
     randomNumber = rand() % maxNumber + 1; // 1-maxnum väliltä arvotaan lukuja
-    //cout << satunnaisluku << endl;
-    //Yllä olevalla rivillä testasin printaamalla, että tulee arvotuksi oikeanlainen luku
+
 
     do {
         cout << "Give your guess between 1 and " << maxNumber << "." << endl;
@@ -36,7 +38,7 @@ void Game::play()
         if (playerGuess < randomNumber) {
             cout << "Your guess is too small." << endl;
         } else if (playerGuess > randomNumber) {
-            cout << "Your guess is too big.";
+            cout << "Your guess is too big." << endl;
         }
     } while (playerGuess != randomNumber);
 
@@ -47,6 +49,7 @@ void Game::play()
 void Game::printGameResult()
 {
     cout << "You guessed the right number = " << randomNumber << " with " << numOfGuesses << " guesses!" << endl;
+    cout << "Debuggaus-tulostusta. Tama oli Game::printGameResult() -funktio." << endl;
 }
 
 
