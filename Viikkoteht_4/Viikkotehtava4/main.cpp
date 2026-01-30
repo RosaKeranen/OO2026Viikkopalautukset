a3d9785fef3b009b12dec549c55ad174353ccdd4
RosaKeranen <rosa.vilkama@gmail.com>
2026-01-30 15:21:14 +0200

    Some more corrections

--- Viikkoteht_4/Viikkotehtava4/main.cpp
+++ Viikkoteht_4/Viikkotehtava4/main.cpp
@@ -7,13 +7,17 @@ using namespace std;
 int main()
 {
     Asiakas asiakas1("Aapeli", 1000);
-    asiakas1.talletus(200);
+    asiakas1.talletus(2000);
     Asiakas asiakas2("Bertta", 2000);
     asiakas2.talletus(100);
 
-    Asiakas & vastaanOttaja = asiakas2;
-    asiakas1.tiliSiirto(50, vastaanOttaja);
+    Asiakas* vastaanottaja = &asiakas2; //mainissa tehdään osoitin Asiakas-olioon
+    asiakas1.tiliSiirto(-598, *vastaanottaja);
 
+    vastaanottaja = &asiakas1; //vastaanottaja vaihtuu
+    asiakas2.tiliSiirto(20, *vastaanottaja);
+
+    asiakas1.showSaldo();
     asiakas2.showSaldo();
 
 }
