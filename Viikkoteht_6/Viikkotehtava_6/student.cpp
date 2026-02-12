f388a4a88e79a4e69e5d9db9b0264937b0f7ac28
RosaKeranen <rosa.vilkama@gmail.com>
2026-02-12 12:43:24 +0200

    Week task 6 started

---
+++ Viikkoteht_6/Viikkotehtava_6/student.cpp
@@ -0,0 +1,34 @@
+#include "student.h"
+
+Student::Student(string n, int a)
+{
+    name = n;
+    age = a;
+}
+
+void Student::setAge(int studentAge)
+{
+    age = studentAge;
+}
+
+void Student::setName(string studentName)
+{
+    name = studentName;
+}
+
+string Student::getName()
+{
+
+}
+
+int Student::getAge()
+{
+
+}
+
+void Student::printStudentInfo()
+{
+
+}
+
+
